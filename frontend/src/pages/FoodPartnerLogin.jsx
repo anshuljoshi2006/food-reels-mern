427e34a3201cbc8fe1f2ce335ee514ee8e876ab5
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-02-05 17:33:19 -0800

    Fix API import paths for pages

--- frontend/src/pages/FoodPartnerLogin.jsx
+++ frontend/src/pages/FoodPartnerLogin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
-import { API } from '../../api';
+import { API } from '../api';
 
 const PartnerLogin = () => {
 
