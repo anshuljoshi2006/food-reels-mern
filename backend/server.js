244d5c86f34c5f22b42dd4d74e69a6a8771be1ea
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-06-27 14:18:36 +0530

    Added debug logs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-connectDB();
+console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
+
+connectDB()
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("DB Error:", err));
 
 module.exports = app;
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1); // stop server if DB fails
+    throw error;
   }
 };
 
