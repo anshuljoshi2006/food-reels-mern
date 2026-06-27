3cc9b3520b64d74f40cb5477a670a5f521bc4989
Anshul Joshi <anshuljoshi22122006@gmail.com>
2026-06-27 14:22:36 +0530

    revert debug changes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-console.log("Mongo URI exists:", !!process.env.MONGODB_URI);
-
-connectDB()
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("DB Error:", err));
+connectDB();
 
 module.exports = app;
